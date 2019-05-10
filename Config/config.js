b49eef8ef55cf2dd9cf17633d6f575cff912a24b
saravind23399 <saravind23399@gmail.com>
2019-05-10 20:12:06 +0530

    Updated Config to support logging
    Aravind S

--- Config/config.js
+++ Config/config.js
@@ -1,11 +1,19 @@
 const isProduction = false
 
 let productionConfig = {
-    dbUrl: ''
+    dbUrl: process.env.MONGODB_ADDON_URI,
+    log:{
+        errorFile: './Logs/logs.log',
+        rotationPeriod: '4.5d',
+    }
 }
 
 let devConfig = {
-    dbUrl: process.env.MONGODB_ADDON_URI
+    dbUrl: 'mongodb://localhost:27017/postifi',
+    log:{
+        errorFile: './Logs/logs.log',
+        rotationPeriod: '1m',
+    }
 }
 
 module.exports = isProduction?productionConfig:devConfig
