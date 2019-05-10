492f75ccfed368439eeb06832bc9123aaba88f2c
saravind23399 <saravind23399@gmail.com>
2019-05-10 20:12:41 +0530

    Updated with new Logger
    Aravind S

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
+const cors = require('cors')
+const log = require('./Config/logger')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 const config = require('./Config/config')
 
-app.get('', (req, res)=>{
-    res.status(500).json({
-        success: true,
-        message:'Hello World!'
-    })
-})
-
 app.listen(PORT, err => {
     if(err){
-        console.log('Cannot Start Server on PORT')
+        log.fatal('Cannot Start Server on PORT' + PORT)
     } else {
         mongoose.connect(config.dbUrl, {useNewUrlParser: true}, (dbConnectError)=>{
             if(dbConnectError){
-                console.log('Cannot Connect to DB @ ' + config.dbUrl)
+                log.error('Cannot Connect to DB @ ' + config.dbUrl)
             } else {
-                console.log('Server started at PORT  ' + PORT)
-                console.log('Connected to DB @ ' + config.dbUrl)
+                log.info('Server started at PORT  ' + PORT)
+                log.info('Connected to DB @ ' + config.dbUrl)
             }
         })
     }
