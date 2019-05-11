956e1121e3bcfc3cd69e99dcee2f4145b54bd847
saravind23399 <saravind23399@gmail.com>
2019-05-11 14:20:55 +0530

    Added Auth Route
    Aravind S

--- Config/config.js
+++ Config/config.js
@@ -1,19 +1,29 @@
 const isProduction = false
 
 let productionConfig = {
+    production: true,
     dbUrl: process.env.MONGODB_ADDON_URI,
     log:{
         errorFile: './Logs/logs.log',
         rotationPeriod: '4.5d',
     },
+    googleOAuth:{
+        clientId: '271364661089-186f38cugovk271ke8ktvqrmo49gd9ss.apps.googleusercontent.com',
+        secret: 'F30gbFMzB3eX_1mks5tj4qdg'
+    },
     port: process.env.PORT
 }
 
 let devConfig = {
+    production: false,
     dbUrl: 'mongodb://localhost:27017/postifi',
     log:{
         errorFile: './Logs/logs.log',
         rotationPeriod: '1m',
+    },
+    googleOAuth:{
+        clientId: '271364661089-186f38cugovk271ke8ktvqrmo49gd9ss.apps.googleusercontent.com',
+        secret: 'F30gbFMzB3eX_1mks5tj4qdg'
     },
     port: 3000
 }
--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const cors = require('cors')
 const config = require('./Config/config')
 const log = require('./Config/logger')
 
+const authRoute = require('./Routes/auth')
+
 const app = express()
+app.use(cors({origin: 'http://localhost:4200'}))
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use('/auth', authRoute)
 
 app.get('/', (req, res) => {
     res.status(200).json({
