35ad5bddc6f357fc0fe2eb83ae4cfb14993ea5f2
saravind23399 <saravind23399@gmail.com>
2019-05-12 14:44:58 +0530

    Created new Logger
    Aravind S

---
+++ Lib/logger.js
@@ -0,0 +1,56 @@
+const bunyan = require('bunyan')
+const config = require('./config')
+
+const levels = {
+    error: 0,
+    warning: 1,
+    debug: 2,
+
+};
+
+let getRotatingFileStream = (level, fileName) => {
+    return {
+        level: level,
+        type: 'rotating-file',
+        path: fileName,
+        period: config.log.rotationPeriod
+    }
+}
+
+let reqSerializer = (req) => {
+    return {
+         method: req.method,
+         url: req.originalUrl,
+         headers: req.headers,
+     }
+ }
+
+const errorLogger = bunyan.createLogger({
+    name: 'Postifi-Backend-Error',
+    serializers: {
+        request: reqSerializer,
+        info: (info)=>{return info}
+    },
+    streams: [
+        getRotatingFileStream('error', config.log.errorFile),
+    ]
+});
+
+const infoLogger = bunyan.createLogger({
+    name: 'Postifi-Backend-Info',
+    streams: [
+        {
+            level: 'info',
+            stream: process.stdout
+        }
+    ]
+});
+
+module.exports = {
+    fatal: errorLogger.fatal.bind(errorLogger),
+    error: errorLogger.error.bind(errorLogger),
+    warn: infoLogger.warn.bind(infoLogger),
+    info: infoLogger.info.bind(infoLogger),
+    debug: infoLogger.debug.bind(infoLogger),
+    trace: infoLogger.trace.bind(infoLogger)
+}
