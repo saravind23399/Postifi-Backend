14aa083c66764c60df44576117ac845e85cb8fd2
saravind23399 <saravind23399@gmail.com>
2019-05-12 14:42:20 +0530

    Added Comment Field
    Aravind S

--- app.js
+++ app.js
@@ -2,21 +2,26 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const cors = require('cors')
 
-const config = require('./Config/config')
-const log = require('./Config/logger')
+const config = require('./Lib/config')
+const log = require('./Lib/logger')
 
 const authRoute = require('./Routes/auth')
+const postRoute = require('./Routes/post')
+const commentRoute = require('./Routes/comment')
 
 const app = express()
+
 app.use(cors({origin: 'http://localhost:4200'}))
 
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({ extended: true }));
 
 app.use('/auth', authRoute)
+app.use('/post', postRoute)
+app.use('/comment', commentRoute)
 
 app.get('/', (req, res) => {
-    res.status(200).json({
+    res.status(202).json({
         success: true,
         message: 'Server Live!'
     })
