0da03d0a557fd770d127c99b2b833424c139bebd
saravind23399 <saravind23399@gmail.com>
2019-05-12 14:44:49 +0530

    Added Links and Name for the Post
    Aravind S

--- Models/post.js
+++ Models/post.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose')
 
 const PostSchema = mongoose.Schema({
+    name: String,
     email: String,
     postTitle: String,
     postContent: String,
-}, { 
-    timestamps: { 
-        createdAt: 'createdAt',
-        updatedAt: 'updatedAt' 
-    }
-)
+    postImageUrl: String,
+    postHyperLinkUrl: String
+}, {
+        timestamps: {
+            createdAt: 'createdAt',
+            updatedAt: 'updatedAt'
+        }
+    })
 
-export mongoose.model('Post', PostSchema)
+module.exports = mongoose.model('Post', PostSchema)
