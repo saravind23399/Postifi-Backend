9ff9ad8de118b8c15d6524c90470095e18c4111e
saravind23399 <saravind23399@gmail.com>
2019-05-12 14:44:28 +0530

    Bug Fixes
    Aravind S

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,33 +1,37 @@
-const router = require('express').Router()
-const googleVerifier = require('google-id-token-verifier')
-const config = require('../Config/config')
-const log = require('../Config/logger')
+const handler = require('express').Router()
+const config = require('../Lib/config')
+const log = require('../Lib/logger')
 
-router.get('/ping', (req, res) => {
-    res.json({
+const googleAuthLib = require('../Lib/googleAuth')
+
+handler.get('/ping', (req, res) => {
+    res.status(202).json({
         success: true,
         message: 'Server Live'
     })
 })
 
-router.post('/google/resolveToken', (req, res) => {
-    googleVerifier.verify(req.body.idToken, config.googleOAuth.clientId, (tokenError, verifiedToken) => {
-        if (tokenError) {
-            log.error('Cannot verifiy Google idToken : ' + req.body.idToken + ' | Reason : ' + tokenError)
-            res.status(500).json({
+handler.post('/google/resolveToken', (req, res) => {
+    googleAuthLib.verifyGoogleIdToken(req, (tokenError, verifiedToken)=>{
+        if(tokenError){
+            log.error({ request: req, info: 'Unable to verify Token | TokenError : ' + tokenError })
+            res.status(406).json({
                 success: false,
-                message: 'Cannot verify token',
+                message: 'Cannot verify Google Token. Logout and try again',
                 debug: config.production?null:tokenError
             })
         } else {
-            log.info('Token verified successfully : ' + req.body.idToken)
-            res.status(200).json({
+            res.status(202).json({
                 success: true,
-                message: docs,
-                debug: config.production?null:verifiedToken               
+                message: {
+                    email: verifiedToken.email,
+                    picture: verifiedToken.picture,
+                    name: verifiedToken.name
+                },
+                debug: config.production?null:tokenError
             })
         }
     })
 })
 
-module.exports = router
+module.exports = handler
--- Routes/post.js
+++ Routes/post.js
@@ -1,10 +1,257 @@
-const router = require('express').Router()
+const handler = require('express').Router()
+const post = require('../Models/post')
 
-router.get('/ping', (req, res)=>{
-    res.json({
+const log = require('../Lib/logger')
+const config = require('../Lib/config')
+const googleAuthLib = require('../Lib/googleAuth')
+
+handler.get('/ping', (req, res) => {
+    res.status(202).json({
         success: true,
         message: 'Server Live'
     })
 })
 
-module.exports = router
+handler.post('/newPost', (req, res) => {
+    if (!req.body.idToken) {
+        log.error({ request: req, info: 'Request without idToken' })
+        res.status(406).json({
+            success: false,
+            message: 'User Not Authenticated. No Google idToken.'
+        })
+    } else {
+        googleAuthLib.verifyGoogleIdToken(req, (tokenError, verifiedToken) => {
+            if (tokenError) {
+                res.status(406).json({
+                    success: false,
+                    message: 'Unable to validate user. Please login again. ',
+                    debug: config.production ? null : tokenError
+                })
+            } else {
+                if (req.body.postTitle && req.body.postContent && req.body.postImageUrl && req.body.postHyperLinkUrl && req.body._id) {
+                    const newPost = new post({
+                        name: verifiedToken.name,
+                        email: verifiedToken.email,
+                        postTitle: req.body.postTitle,
+                        postContent: req.body.postTitle,
+                        postImageUrl: req.body.postImageUrl,
+                        postHyperLinkUrl: req.body.postHyperLinkUrl
+                    })
+                    newPost.save((saveError, saveDocs) => {
+                        if (saveError) {
+                            log.error({ request: req, info: 'Unable to save post : ' + saveError })
+                            res.status(406).json({
+                                success: false,
+                                message: 'An Error occured. Try again',
+                                debug: config.production ? null : saveError
+                            })
+                        } else {
+                            log.info({ request: req, info: 'New post added | Title : ' + req.body.postTitle + ' by ' + verifiedToken.email })
+                            res.status(202).json({
+                                success: true,
+                                message: 'Posted successfully!',
+                                debug: config.production ? null : saveDocs
+                            })
+                        }
+                    })
+                } else {
+                    log.error({ request: req, info: 'Incomplete body for new post' })
+                    res.status(406).json({
+                        success: false,
+                        message: 'Invalid/Incomplete Values for adding a new post.',
+                        debug: config.production ? null : req.body
+                    })
+                }
+            }
+        })
+    }
+})
+
+handler.get('/editPost', (req, res) => {
+    if (!req.body.idToken) {
+        log.error({ request: req, info: 'Request without idToken' })
+        res.status(406).json({
+            success: false,
+            message: 'User Not Authenticated. No Google idToken.'
+        })
+    } else {
+        googleAuthLib.verifyGoogleIdToken(req, (tokenError, verifiedToken) => {
+            if (tokenError) {
+                res.status(406).json({
+                    success: false,
+                    message: 'Unable to validate user. Please login again. ',
+                    debug: config.production ? null : tokenError
+                })
+            } else {
+                if (req.body.postTitle && req.body.postContent && req.body.postImageUrl && req.body.postHyperLinkUrl && req.body._id) {
+                    post.find({ email: verifiedToken.email, _id: req.body._id }, (postFindError, postFindDocs) => {
+                        if (postFindError) {
+                            log.error({ request: req, info: 'Error while searching Post : ' + postFindError })
+                            res.status(406).json({
+                                success: false,
+                                message: 'Error while searching Post ',
+                                debug: config.production ? null : postFindError
+                            })
+                        } else {
+                            if (postFindDocs.length == 0) {
+                                log.error({ request: req, info: 'No post find found | email : ' + req.body.email + '| _id : ' + req.body._id })
+                                res.status(406).json({
+                                    success: false,
+                                    message: 'Error while searching Post ',
+                                    debug: config.production ? null : postFindDocs
+                                })
+                            } else {
+                                post.findByIdAndUpdate(req.body._id, {
+                                    $set: {
+                                        postTitle: req.body.postTitle,
+                                        postContent: req.body.postTitle,
+                                        postImageUrl: req.body.postImageUrl,
+                                        postHyperLinkUrl: req.body.postHyperLinkUrl
+                                    }
+                                }, (updateError, updateDocs) => {
+                                    if (updateError) {
+                                        log.error({ request: req, info: 'Cannot update post | _id : ' + req.body._id })
+                                        res.status(202).json({
+                                            success: false,
+                                            message: 'Error while updating Post ',
+                                            debug: config.production ? null : updateError
+                                        })
+                                    } else {
+                                        log.info({ request: req, info: 'Post Updated | _id : ' + req.body._id })
+                                        res.status(202).json({
+                                            success: true,
+                                            message: 'Post updated Successfully ',
+                                            debug: config.production ? null : updateDocs
+                                        })
+                                    }
+                                })
+                            }
+                        }
+                    })
+                } else {
+                    log.error({ request: req, info: 'Incomplete body for editing post' })
+                    res.status(406).json({
+                        success: false,
+                        message: 'Invalid/Incomplete Values for editing the post.',
+                        debug: config.production ? null : req.body
+                    })
+                }
+            }
+        })
+    }
+})
+
+handler.post('/deletePost', (req, res) => {
+    if (!req.body.idToken) {
+        log.error({ request: req, info: 'Request without idToken' })
+        res.status(406).json({
+            success: false,
+            message: 'User Not Authenticated. No Google idToken.'
+        })
+    } else {
+        googleAuthLib.verifyGoogleIdToken(req, (tokenError, verifiedToken) => {
+            if (tokenError) {
+                res.status(406).json({
+                    success: false,
+                    message: 'Unable to validate user. Please login again. ',
+                    debug: config.production ? null : tokenError
+                })
+            } else {
+                if (req.body._id) {
+                    post.find({ email: verifiedToken.email, _id: req.body._id }, (postFindError, postFindDocs) => {
+                        if (postFindError) {
+                            log.error({ request: req, info: 'Error while searching Post : ' + postFindError })
+                            res.status(406).json({
+                                success: false,
+                                message: 'Error while searching Post ',
+                                debug: config.production ? null : postFindError
+                            })
+                        } else {
+                            if (postFindDocs.length == 0) {
+                                log.error({ request: req, info: 'No post find found | email : ' + verifiedToken.email + '| _id : ' + req.body._id })
+                                res.status(406).json({
+                                    success: false,
+                                    message: 'Error while searching Post ',
+                                    debug: config.production ? null : postFindDocs
+                                })
+                            } else {
+                                post.findByIdAndDelete(req.body._id, (deleteError, deleteDocs) => {
+                                    if (deleteError) {
+                                        log.error({ request: req, info: 'Cannot delete post | _id : ' + req.body._id })
+                                        res.status(202).json({
+                                            success: false,
+                                            message: 'Error while deleting Post ',
+                                            debug: config.production ? null : deleteError
+                                        })
+                                    } else {
+                                        log.info({ request: req, info: 'Post Deleted | _id : ' + req.body._id })
+                                        res.status(202).json({
+                                            success: true,
+                                            message: 'Post deleted Successfully ',
+                                            debug: config.production ? null : deleteDocs
+                                        })
+                                    }
+                                })
+                            }
+                        }
+                    })
+                } else {
+                    log.error({ request: req, info: 'Incomplete body for deleting post' })
+                    res.status(406).json({
+                        success: false,
+                        message: 'Invalid/Incomplete Values for deleted the post.',
+                        debug: config.production ? null : req.body
+                    })
+                }
+            }
+        })
+    }
+})
+
+handler.get('/getAllPosts', (req, res) => {
+    post.find({}).sort({ updatedAt: -1 }).exec((findError, findDocs) => {
+        if (findError) {
+            log.error({ request: req, info: 'Error while getting all posts' })
+            res.status(406).json({
+                success: false,
+                message: 'Database Error. Try again',
+                debug: config.production ? null : findError
+            })
+        } else {
+            res.status(202).json({
+                success: true,
+                message: findDocs
+            })
+        }
+    })
+})
+
+handler.post('/getPost', (req, res) => {
+    if (req.body._id) {
+        post.findById(req.body._id, (findError, findDocs) => {
+            if (findError) {
+                log.error({ request: req, info: 'Error while getting post : ' + req.body._id })
+                res.status(406).json({
+                    success: false,
+                    message: 'Error while getting post',
+                    debug: findError
+                })
+            } else {
+                res.status(202).json({
+                    success: true,
+                    message: findDocs,
+                    debug: findError
+                })
+            }
+        })
+    } else {
+        log.error({ request: req, info: 'Error while getting all posts' })
+        res.status(406).json({
+            success: false,
+            message: 'No post id provided',
+            debug: req.body
+        })
+    }
+})
+
+module.exports = handler
